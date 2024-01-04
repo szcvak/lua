d3c2555789c0694d4098a3cadae7ba66559e18a3
szcvak <142408909+szcvak@users.noreply.github.com>
2024-01-04 19:50:02 +0100

    Add files via upload

--- Spotlight.lua
+++ Spotlight.lua
@@ -88,7 +88,7 @@ end
 
 function Spotlight:Show()
 	self.Entry.Text = ""
-	self.Container.Position = UDim2.new(0, Mouse.X + 15, 0, Mouse.Y - 15)
+	self.Container.Position = UDim2.new(0, Mouse.X + 10, 0, Mouse.Y + 10)
 	self.Container.Visible = true
 	self.Entry:CaptureFocus()
 end
